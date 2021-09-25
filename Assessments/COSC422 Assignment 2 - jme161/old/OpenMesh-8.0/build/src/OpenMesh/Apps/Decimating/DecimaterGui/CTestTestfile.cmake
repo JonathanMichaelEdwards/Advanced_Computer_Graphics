45e39d25daebf1e70ebc097d243a798457a0d8e8
JonathanMichaelEdwards <jon.michael.edwards13@gmail.com>
2021-09-26 12:10:56 +1300

    Assessment 2 layout

---
+++ Assessments/COSC422 Assignment 2 - jme161/old/OpenMesh-8.0/build/src/OpenMesh/Apps/Decimating/DecimaterGui/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jonathan/Desktop/OpenMesh-8.0/src/OpenMesh/Apps/Decimating/DecimaterGui
+# Build directory: /home/jonathan/Desktop/OpenMesh-8.0/build/src/OpenMesh/Apps/Decimating/DecimaterGui
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
