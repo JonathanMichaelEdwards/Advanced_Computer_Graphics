5131597693cd894899bab74fcb273c8fa9344285
JonathanMichaelEdwards <jon.michael.edwards13@gmail.com>
2021-09-26 13:21:29 +1300

    del

--- Assessments/COSC422 Assignment 2 - jme161/old/OpenMesh-8.0/build/src/OpenMesh/Apps/VDProgMesh/Analyzer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jonathan/Desktop/OpenMesh-8.0/src/OpenMesh/Apps/VDProgMesh/Analyzer
-# Build directory: /home/jonathan/Desktop/OpenMesh-8.0/build/src/OpenMesh/Apps/VDProgMesh/Analyzer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
